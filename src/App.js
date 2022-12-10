70dd1b695e819b62ef27f598f614eb5c8942a6c0
thiagosaidm <thiag.said@gmail.com>
2022-12-10 11:22:36 -0300

    "#"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import logo from './assets/logo.png'
 
 
 function App() {
   return (
-    <h1>Hello world</h1>
+    <div className="container">
+      <div className="container_login">
+        <div className="wrap_login">
+          <form className="login_form">
+              <span className="form_title">
+                  Olá! Seja bem vindo!
+              </span>
+              <span className="form_title">
+                <img src={logo} alt="Logo"/>
+              </span>
 
+            <div className='wrap-input'>
+                <input type="email"/>
+                <span className="focus_input" data-placeholder="E-mail" ></span>
+            </div>
+            <div className='wrap-input'>
+                <input type="password" />
+                <span className="focus_input" data-placeholder="Senha" ></span>
+            </div>
+            
+
+
+
+            </form>
+          </div>
+      </div>
+    </div>
     );
 }
 
